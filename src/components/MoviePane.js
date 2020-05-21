839279dbbbc32dc08ea9850a38b47e36099e9dc9
Godwin254 <godwinbee15@gmail.com>
2020-05-20 23:18:20 -0400

    modified MoviePane component

---
+++ src/components/MoviePane.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MoviePane extends Component {
+     render() {
+          return (
+               <div>
+                  <h3>Movie Pane</h3>  
+               </div>
+          )
+     }
+}
+
+export default MoviePane
